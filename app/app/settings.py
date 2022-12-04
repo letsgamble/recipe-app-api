2ffcb2c2000e95e6b40d8e8754ad60a3c8e11e4e
Kamil <letsgamble92@gmail.com>
2022-12-04 15:44:03 +0100

    Adding drf_spectacular support.

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
